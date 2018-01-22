034def6282cb1cae355c659c666ffcef26de992f
Glitch (book-trading-club) <none>
2018-01-22 17:55:13 +0000

    😎🌒 Updated with Glitch

---
+++ app/components/Main.jsx
@@ -0,0 +1,13 @@
+const React = require('react');
+const Link = require('react-router-dom').Link
+
+/* the main page for the index route of this app */
+const Main = function() {
+  return (
+    <div>
+      <h1>Hello World!</h1>
+    </div>
+  );
+};
+
+module.exports = Main;
